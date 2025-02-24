0df12810ef22d9b6ed11113567497d747280b007
parthgoyal13 <nparthgoyal.skncoe.comp@gmail.com>
2025-02-24 15:44:08 +0530

    added search by title and tag

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React,{useState} from "react";
 import { Link } from "react-router-dom";
 
 
-const Header=()=>{
+const Header=({ onSearch })=>{
+  const [searchQuery, setSearchQuery] = useState("")
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+    onSearch(e.target.value); 
+  };
   return(
     <>
     <header className="d-flex justify-content-between align-items-center p-3 bg-light">
     <h3><Link to="/" className="text-dark">Meetup</Link>
     </h3>
     <div className="input-group w-50">
-    <input type="text" placeholder="Search by title and tags" className="form-control"/>
+
+      
+    <input type="text" 
+    placeholder="Search by title and tags" className="form-control"
+    value={searchQuery}
+    onChange={handleSearchChange} />
     </div>
     </header>
     <hr />
--- src/pages/ListingPage.jsx
+++ src/pages/ListingPage.jsx
@@ -4,18 +4,31 @@ import useFetch from "../useFetch";
 import EventCard from "../components/EventCard";
 
 const ListingPage=()=>{
-  const [eventType, setEventType] = useState(""); 
-  const { data, loading, error } = useFetch(  eventType ? `https://meetup-backend-lac.vercel.app/events/${eventType}` : `https://meetup-backend-lac.vercel.app/readAllEvents`);
+  const [eventType, setEventType] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+  
+  const apiUrl = searchQuery
+  ? `https://meetup-backend-lac.vercel.app/event/titleAndTag/${searchQuery}/${searchQuery}`
+  : eventType
+  ? `https://meetup-backend-lac.vercel.app/events/${eventType}`
+  : `https://meetup-backend-lac.vercel.app/readAllEvents`;
 
-
+  const { data, loading, error } = useFetch(apiUrl);
   const handleEventTypeChange = (e) => {
     setEventType(e.target.value); 
+    setSearchQuery("");
   };
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+    setEventType(""); 
+  };
+
   if (loading) return <p >Loading...</p>;
   if (error) return <p>{error}</p>;
   return(
     <>
-    <Header/>
+    <Header onSearch={handleSearch}/>
     <div className="container mt-4">
     <div className="d-flex justify-content-between align-items-center mb-4">
       <h2 className="mb-0">Meetup Events</h2>
