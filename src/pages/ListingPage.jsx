f44428514c43d96bc50ac6216cc4931eb4834b25
parthgoyal13 <nparthgoyal.skncoe.comp@gmail.com>
2025-02-24 14:21:20 +0530

    added functionlaity to show event type

--- src/pages/ListingPage.jsx
+++ src/pages/ListingPage.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import useFetch from "../useFetch";
 import EventCard from "../components/EventCard";
 
 const ListingPage=()=>{
+  const [eventType, setEventType] = useState(""); 
+  const { data, loading, error } = useFetch(  eventType ? `https://meetup-backend-lac.vercel.app/events/${eventType}` : `https://meetup-backend-lac.vercel.app/readAllEvents`);
 
-  const { data, loading, error } = useFetch("https://meetup-backend-lac.vercel.app/readAllEvents");
 
+  const handleEventTypeChange = (e) => {
+    setEventType(e.target.value); 
+  };
   if (loading) return <p >Loading...</p>;
-
+  if (error) return <p>{error}</p>;
   return(
     <>
     <Header/>
@@ -16,22 +20,26 @@ const ListingPage=()=>{
     <div className="d-flex justify-content-between align-items-center mb-4">
       <h2 className="mb-0">Meetup Events</h2>
 
-      <select  id="typesOfEvents" className="form-select w-auto">
+      <select  id="typesOfEvents" className="form-select w-auto" onChange={handleEventTypeChange}>
         <option value="">Select Event Type</option>
         <option value="Online">Online</option>
         <option value="Offline">Offline</option>
-        <option value="">Both</option>
+        <option value="Both">Both</option>
       </select>
     </div>
 
     <div className="row">
-      {data?.map((event)=>(
-        <div className="col-md-4" key={event._id}>
-          <EventCard event={event} />
+          {data?.length > 0 ? (
+            data.map((event) => (
+              <div className="col-md-4" key={event._id}>
+                <EventCard event={event} />
+              </div>
+            ))
+          ) : (
+            <p>No events found.</p>
+          )}
         </div>
-      ))}
-    </div>
-    </div>
+      </div>
     </>
   )
 }
